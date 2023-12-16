f8fc1a3b4f0e1865a3575042253b258ebd0e0009
KIRAN-KUMAR-K3 <18kirankumar.k03@gmail.com>
2023-12-16 13:05:20 +0530

    corrected optional output mistakes

--- game.sh
+++ game.sh
@@ -2,55 +2,44 @@
 
 commands=("ls" "pwd" "cd" "cp" "mv" "rm" "mkdir" "rmdir" "cat" "echo" "grep" "chmod" "man" "ps" "kill")
 
-score=0
-level=1
-
-function display_intro() {
-    clear
-    echo -e "\e[1;36mWelcome to the Linux Command Game!\e[0m"
-    echo -e "\e[1;33mGuess the Linux command based on its description.\e[0m"
-    echo -e "\e[1;32mType 'exit' to quit the game.\e[0m"
-    echo
-}
-
-function display_score() {
-    echo -e "\e[1;35mScore: $score\e[0m"
-    echo -e "\e[1;35mLevel: $level\e[0m"
-}
-
 function play_game() {
-    local random_command=${commands[$((RANDOM % ${#commands[@]}))]}
-    local description=$(grep -oP "(?<=^$random_command': ').*" "$0")  # Extract description from the script
+    current_command=${commands[$((RANDOM % ${#commands[@]}))]}
 
-    echo -e "\e[1;34mDescription: $description\e[0m"
-    user_guess=""
-    
-    read -p "Your guess: " user_guess
+    user_guess=$(zenity --entry --title="Linux Command Game" --text="Welcome to the Linux Command Game!\nGuess the Linux command based on its description:\n\nDescription: ${descriptions[$current_command]}")
 
-    if [ "$user_guess" == "exit" ]; then
-        echo -e "\e[1;36mThanks for playing! Exiting the game.\e[0m"
-        exit 0
+    if [[ "$user_guess" == "exit" ]]; then
+        quit_game
     fi
 
-    if [ "$user_guess" == "$random_command" ]; then
-        echo -e "\e[1;32mCorrect!\e[0m"
-        ((score++))
+    if [[ "$user_guess" == "$current_command" ]]; then
+        zenity --info --title="Correct" --text="Congratulations! Your guess is correct."
     else
-        echo -e "\e[1;31mIncorrect. The correct command is '$random_command'.\e[0m"
+        zenity --error --title="Incorrect" --text="Oops! The correct command is '$current_command'."
     fi
 
-    if [ "$score" -ge $((level * 3)) ]; then
-        ((level++))
-        echo -e "\e[1;33mCongratulations! You've advanced to level $level.\e[0m"
-    fi
-
-    display_score
-    read -n 1 -s -r -p "Press any key to continue..."
+    play_game
 }
 
-display_intro
-display_score
+function quit_game() {
+    exit 0
+}
 
-while true; do
-    play_game
-done
+declare -A descriptions=(
+    ["ls"]="List directory contents"
+    ["pwd"]="Print working directory"
+    ["cd"]="Change directory"
+    ["cp"]="Copy files or directories"
+    ["mv"]="Move or rename files or directories"
+    ["rm"]="Remove files or directories"
+    ["mkdir"]="Create a directory"
+    ["rmdir"]="Remove an empty directory"
+    ["cat"]="Concatenate and display the content of files"
+    ["echo"]="Display a message or variable"
+    ["grep"]="Search for a pattern in a file"
+    ["chmod"]="Change file permissions"
+    ["man"]="Display manual pages for a command"
+    ["ps"]="List information about running processes"
+    ["kill"]="Terminate or signal processes"
+)
+
+play_game
